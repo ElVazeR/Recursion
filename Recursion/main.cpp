03f79f06b0968f64d5fb6269aff18bee9218f363
К - 9 < - 9@DESKTOP-L08D11R>
2024-04-04 20:53:50 +0300

    Добавьте файлы проекта.

---
+++ Recursion/main.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+// Рекурсия - Это прием программирования, при котором функция вызывает саму себя либо непосредственно, либо косвенно.
+
+unsigned long long factorial(int num) {
+	if (num < 0)
+		return 0;
+	if (num == 0)
+		return 1;
+	return factorial(num - 1) * num;
+}
+
+int fibonacci(int num) {
+	if (num <= 0)
+		return 0;
+	if (num == 1)
+		return 1;
+	return fibonacci(num - 1) + fibonacci(num - 2);
+
+}
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int n;
+
+	/* Задание "Факториал"
+	std::cout << "Введите число -> ";
+	std::cin >> n;
+	std::cout << n << "! = " << factorial(n) << std::endl;
+	*/
+
+	// Задача 1. Числа Фибоначчи
+
+	std::cout << "Введите номер числа Фибоначчи -> ";
+	std::cin >> n;
+	std::cout << fibonacci(n) << std::endl;
+	for (int i = 0; i < 13; i++)
+		std::cout << fibonacci(i) << ' ';
+	std::cout << std::endl;
+
+
+	return 0;
+}
